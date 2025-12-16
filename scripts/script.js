7bca124d52995cdc3a9a0f2097b83f906ca0f1a5
traliach <ali.achille.traore@gmail.com>
2025-12-16 15:08:58 -0500

    add main heading

--- scripts/script.js
+++ scripts/script.js
@@ -3,3 +3,5 @@ console.log("script.js loaded ✅");
 
 // Part 1 — Main content
 const mainEl = document.querySelector("main");
+mainEl.style.backgroundColor = "var(--main-bg)";
+mainEl.innerHTML = "<h1>DOM Manipulation</h1>";
